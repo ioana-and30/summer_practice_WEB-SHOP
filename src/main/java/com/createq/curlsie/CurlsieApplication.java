3be742ebd8797e548077e67777eceabc3331b234
ioana-and30 <ioanaandreea21603@gmail.com>
2025-07-07 12:03:18 +0300

    Initial commit and uploading Intellij project

---
+++ src/main/java/com/createq/curlsie/CurlsieApplication.java
@@ -0,0 +1,13 @@
+package com.createq.curlsie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurlsieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurlsieApplication.class, args);
+    }
+
+}
